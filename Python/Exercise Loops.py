0d3f15a0b1ca9571563688523205a6946da472d5
Abdullah <maabdullah.rhs@gmail.com>
2023-04-07 23:42:35 +0500

    modified:   Python/Exercise Loops.py

--- Python/Exercise Loops.py
+++ Python/Exercise Loops.py
@@ -1,15 +1,14 @@
 n = 0
-m = int(input("Enter the number you want to check prime numbers till that number."))
+m = int(input("Enter the number you want to check prime numbers till that number:"))
 while n<=m:
     n += 1
-    print('The value of {} is in single figure.'.format(n))
     if n%2==1:
         print("This number is an odd integer.")
         divisor = []
         divisor.append(n)
         while n>0:
             prime = []
-            if n%n and n%1 == 0:
+            while n%n==0:
                 if n%divisor == 0:
                     print("The number {} is not a prime integer".format(n))
                 else:
